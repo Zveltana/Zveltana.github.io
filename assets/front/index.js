fb2341a3e3ca85e5c88b5c65e9634dbef4333177
Amélia MASSOT <ameliamassot@gmail.com>
2025-02-13 16:01:43 +0100

    Resolve problem homepage

--- assets/front/index.js
+++ assets/front/index.js
@@ -6,7 +6,7 @@ $(window).scroll(function() {
     }
 });
 
-if (window.location.pathname === '/index.html'){
+if (window.location.pathname === '/'){
     document.addEventListener('DOMContentLoaded', () => {
         const curtain = document.getElementById('curtain');
 
@@ -39,33 +39,4 @@ if (window.location.pathname === '/index.html'){
         setColorAndCloseCurtain('#51665D', 'about.html');
         });
     });
-}
-
-if (window.location.pathname === '/realisations.html'){
-    function openModal(container) {
-    const modal = document.getElementById('imageModal');
-    const modalImage = document.getElementById('modalImage');
-    
-    const image = container.querySelector('img'); 
-    modalImage.src = image.src;  // Charge l'image cliquée dans le modal
-    modal.classList.remove('hidden');  // Affiche le modal
-    modal.classList.add('flex');  // Affiche le modal avec flex pour centrer l'image
-    modal.classList.remove('scale-90'); // Anime l'ouverture de l'image
-
-    document.body.classList.add('modal-open');
-    }
-
-    // Ajouter openModal à window pour le rendre accessible globalement
-    window.openModal = openModal;
-
-    function closeModal() {
-        const modal = document.getElementById('imageModal');
-        modal.classList.add('hidden');  // Cache le modal
-        modal.classList.remove('flex'); // Enlève le display flex
-
-        document.body.classList.remove('modal-open');
-    }
-
-    // Ajouter closeModal à window pour le rendre accessible globalement
-    window.closeModal = closeModal;
 }
